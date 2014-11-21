49aa9eef889e3c4c822bcca42d81c1feb171a043
inerts73 <inerts73@yahoo.com>
2014-11-21 21:32:45 +0800

    Added picture

--- js/app.js
+++ js/app.js
@@ -66,8 +66,10 @@ var loadPagesInfo = function(pages){
       $page.find('.title a').text(response.name).attr('href',response.link);
       $page.find('.about').text(response.about);
       $page.find('.likes').text(response.likes);
-      FB.api(/*輸入圖片連結*/, function(response){
+      FB.api(item.id+'/picture?type=large', function(response){
         // 塞資料到 html 中
+        $page.find('.thumbnail img').attr('src', response.data.url);
+        $page.appendTo(current);
         counter++;
         // 塞完資料以後處理一下斷行
         if(counter===pages.length){
